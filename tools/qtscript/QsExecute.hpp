ba784e4441f1644d1ade449360ce6722ebe479e8
Denis Zalevskiy <denis.zalevskiy@jollamobile.com>
2013-01-21 13:58:03 +0200

    separated engine initialization from application
    it is allowing to reuse engine initialization, intended for qml apps invokers
    
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jollamobile.com>

---
+++ tools/qtscript/QsExecute.hpp
@@ -0,0 +1,41 @@
+#ifndef _QSEXECUTE_HPP_
+#define _QSEXECUTE_HPP_
+
+#include <QtScript>
+#include <QCoreApplication>
+
+#include <stdexcept>
+
+namespace QsExecute
+{
+
+class Error : public std::runtime_error
+{
+public:
+    Error(QString const &s)
+        : std::runtime_error(s.toStdString()),
+          msg(s)
+    { }
+
+    virtual ~Error() throw() {}
+
+    QString msg;
+};
+
+class JsError : public Error
+{
+public:
+    JsError(QScriptEngine &engine, QString const &file)
+        : Error(errorMessage(engine, file)) {}
+
+    static QString errorMessage(QScriptEngine &engine, QString const &file);
+};
+
+typedef QScriptValue (*qscript_file_loader_type)(QString, QScriptEngine &);
+
+qscript_file_loader_type setupEngine
+(QCoreApplication &app, QScriptEngine &engine);
+
+}
+
+#endif // _QSEXECUTE_HPP_
