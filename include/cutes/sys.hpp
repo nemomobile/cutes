bc1f50b012ba8d70940780f542ce71e513a85db4
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2013-07-18 15:14:05 +0300

    using Arg() and local self
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- include/cutes/sys.hpp
+++ include/cutes/sys.hpp
@@ -43,7 +43,7 @@ public:
     typedef QFile base_type;
 
     File(v8::Arguments const& args)
-        : base_type(ValueFromV8<QString>(V8ENGINE(), args[0]))
+        : base_type(Arg<QString>(args, 0))
     {
     }
 
@@ -52,31 +52,35 @@ public:
     static v8::Handle<v8::Value> open(const v8::Arguments &args)
     {
         return callConvertException([&args]() {
-            auto p0 = ValueFromV8<int>(V8ENGINE(), args[0]);
-            auto mode = static_cast<QIODevice::OpenModeFlag>(p0);
-            return ValueToV8(QObjFromV8This<base_type>(args)->open(mode));
+                auto self = QObjFromV8This<base_type>(args);
+                auto p0 = Arg<int>(args, 0);
+                auto mode = static_cast<QIODevice::OpenModeFlag>(p0);
+                return ValueToV8(self->open(mode));
             });
     }
 
     static v8::Handle<v8::Value> isOpen(const v8::Arguments &args)
     {
         return callConvertException([&args]() {
-                return ValueToV8(QObjFromV8This<base_type>(args)->isOpen());
+                auto self = QObjFromV8This<base_type>(args);
+                return ValueToV8(self->isOpen());
             });
     }
 
     static v8::Handle<v8::Value> write(const v8::Arguments &args)
     {
         return callConvertException([&args]() {
-                auto p0 = ValueFromV8<QByteArray>(V8ENGINE(), args[0]);
-                return ValueToV8(QObjFromV8This<base_type>(args)->write(p0));
+                auto self = QObjFromV8This<base_type>(args);
+                auto p0 = Arg<QByteArray>(args, 0);
+                return ValueToV8(self->write(p0));
             });
     }
 
     static v8::Handle<v8::Value> close(const v8::Arguments &args)
     {
         return callConvertException([&args]() {
-                QObjFromV8This<base_type>(args)->close();
+                auto self = QObjFromV8This<base_type>(args);
+                self->close();
                 return v8::Undefined();
             });
     }
@@ -100,15 +104,15 @@ public:
     typedef QFileInfo base_type;
 
     FileInfo(v8::Arguments const& args)
-        : base_type(ValueFromV8<QString>(V8ENGINE(), args[0]))
+        : base_type(Arg<QString>(args, 0))
     {
-        qDebug() << "FileInfo ";
     }
 
     static v8::Handle<v8::Value> exists(const v8::Arguments &args)
     {
         return callConvertException([&args]() {
-                return ValueToV8(QObjFromV8This<base_type>(args)->exists());
+                auto self = QObjFromV8This<base_type>(args);
+                return ValueToV8(self->exists());
             });
     }
 
