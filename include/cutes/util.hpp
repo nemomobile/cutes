fbf3b4a7adb12c233d24bfb9d8f79504cb171875
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2013-07-22 18:50:17 +0300

    license headers
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- include/cutes/util.hpp
+++ include/cutes/util.hpp
@@ -1,5 +1,28 @@
 #ifndef _CUTES_JS_UTIL_HPP_
 #define _CUTES_JS_UTIL_HPP_
+/*
+ * Support for cutes on Qt5V8
+ *
+ * Copyright (C) 2013 Jolla Ltd.
+ * Contact: Denis Zalevskiy <denis.zalevskiy@jolla.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ */
 
 #include <QtQml/private/qv8engine_p.h>
 #include <QtQml/private/qjsconverter_p.h>
